0f3bc1d29b09865c449226f2070c8a8d8acca645
firgia <m.firgia1@gmail.com>
2021-04-29 21:40:27 +0700

    menambahkan effect ragdoll

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -5,39 +5,64 @@ using Utils;
 
 namespace AI
 {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D),typeof(CapsuleCollider2D))]
     public abstract class Enemy : MonoBehaviour
-    {
-        
-        void Start()
-        {
-        
-        }
+    {   
+        [Header("Setting Ragdoll")]
+        [SerializeField] private GameObject[] bodyPart;
+        [HideInInspector]  public CapsuleCollider2D cCollider;
+        [HideInInspector] public Rigidbody2D rb;
 
-        
-        void Update()
-        {
-        
-        }
+        private List<CapsuleCollider2D> _ragdollCollider = new List<CapsuleCollider2D>();
+        private List<Rigidbody2D> _ragdollRb = new List<Rigidbody2D>();
 
-
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void Awake()
         {
-            if (collision.gameObject.CompareTag(TagUtils.Bullet) || collision.gameObject.CompareTag(TagUtils.SmasherObstacle))
+            cCollider = GetComponent<CapsuleCollider2D>();
+            rb = GetComponent<Rigidbody2D>();
+
+            foreach(GameObject obj in bodyPart)
             {
-                DeathUsingRagdoll();
+                CapsuleCollider2D cc = obj.GetComponent<CapsuleCollider2D>();
+                Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
+
+                if(cc != null) _ragdollCollider.Add(cc);
+                if(rb != null) _ragdollRb.Add(rb);
             }
+        }
+
+        /// <summary>
+        /// mematikan effect ragdoll 
+        /// </summary>
+        public void DisableRagdoll()
+        {
+            rb.gravityScale = 1;
+
+            cCollider.enabled = true;
+
+            foreach(CapsuleCollider2D cc in _ragdollCollider) cc.enabled = false;
+            foreach (Rigidbody2D rb in _ragdollRb) rb.isKinematic = true;
+        }
+
+        /// <summary>
+        /// menjalankan effect ragdoll
+        /// </summary>
+        public void EnableRagdoll()
+        {
+            rb.gravityScale = 0;
+            cCollider.enabled = false;
+            foreach (CapsuleCollider2D cc in _ragdollCollider) cc.enabled = true;
+            foreach (Rigidbody2D rb in _ragdollRb) rb.isKinematic = false;
         }
 
   
         /// <summary>
         /// di gunakan jika dibutuhkan efect phisic ketika musuh mati
         /// </summary>
-        private void DeathUsingRagdoll()
+        public void DeathUsingRagdoll()
         {
-            /// nanti di isi effect ragdoll
-            Destroy(gameObject);
+            EnableRagdoll();
+            Destroy(gameObject,3);
         }
     }
-
 }
--- Assets/Scripts/Character/Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Character/Enemy/EnemyPatrol.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
 namespace AI
 {
@@ -9,13 +10,22 @@ namespace AI
         // Start is called before the first frame update
         void Start()
         {
-        
+            DisableRagdoll();
         }
 
         // Update is called once per frame
         void Update()
         {
         
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag(TagUtils.Bullet)||
+                collision.gameObject.CompareTag(TagUtils.SmasherObstacle))
+            {
+                DeathUsingRagdoll();
+            }
         }
     }
 }
